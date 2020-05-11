5416ae643e415817318315ef8e604db870943bbd
Zack <warrior.killz@gmail.com>
2020-05-12 00:46:42 +0800

    Completed up till part 4

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zack/Documents/MyProjects/Python/Django/Tutorial/restTutorial/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
